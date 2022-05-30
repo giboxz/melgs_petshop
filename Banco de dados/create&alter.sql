e146192e67d65cdbf1921143077c331b0e6e841d
giboxz <gilvanfsfilho@fac.pe.senac.br>
2022-05-30 00:39:29 -0300

    conclusão banco de dados e criação de dados

--- Banco de dados/create&alter.sql
+++ Banco de dados/create&alter.sql
@@ -11,7 +11,7 @@ CREATE TABLE petshop (
 
 CREATE TABLE produto (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(50),
+    nome TEXT,
     descricao TEXT,
     categoria VARCHAR(50),
     valor REAL
@@ -114,3 +114,67 @@ CREATE TABLE animal_cliente (
     id_animal INTEGER NOT NULL,
     id_cliente INTEGER NOT NULL
 );
+
+ALTER TABLE consulta ADD CONSTRAINT fk_id_petshop_consulta
+    FOREIGN KEY (id_petshop)
+    REFERENCES petshop(id);
+
+ALTER TABLE consulta ADD CONSTRAINT fk_id_animal_consulta
+    FOREIGN KEY (id_animal)
+    REFERENCES animal(id);
+
+ALTER TABLE consulta ADD CONSTRAINT fk_id_veterinario_consulta
+    FOREIGN KEY (id_veterinario)
+    REFERENCES veterinario(id);
+
+ALTER TABLE registro_exame ADD CONSTRAINT fk_id_animal_registro_exame
+    FOREIGN KEY (id_animal)
+    REFERENCES animal(id);
+
+ALTER TABLE registro_exame ADD CONSTRAINT fk_id_exame_registro_exame
+    FOREIGN KEY (id_exame)
+    REFERENCES exame(id);
+
+ALTER TABLE estoque ADD CONSTRAINT fk_id_produto_estoque
+    FOREIGN KEY (id_produto)
+    REFERENCES produto(id);
+
+ALTER TABLE estoque ADD CONSTRAINT fk_id_petshop_estoque
+    FOREIGN KEY (id_petshop)
+    REFERENCES petshop(id);
+
+ALTER TABLE venda ADD CONSTRAINT fk_id_produto_venda
+    FOREIGN KEY (id_produto)
+    REFERENCES produto(id);
+
+ALTER TABLE venda ADD CONSTRAINT fk_id_cliente_venda
+    FOREIGN KEY (id_cliente)
+    REFERENCES cliente(id);
+
+ALTER TABLE venda ADD CONSTRAINT fk_id_petshop_venda
+    FOREIGN KEY (id_petshop)
+    REFERENCES petshop(id);
+
+ALTER TABLE petshop_veterinario ADD CONSTRAINT fk_id_petshop_veterinario
+    FOREIGN KEY (id_petshop)
+    REFERENCES petshop(id);
+
+ALTER TABLE petshop_veterinario ADD CONSTRAINT fk_id_veterinario_veterinario
+    FOREIGN KEY (id_veterinario)
+    REFERENCES veterinario(id);
+
+ALTER TABLE petshop_exame ADD CONSTRAINT fk_id_petshop_petshop_exame
+    FOREIGN KEY (id_petshop)
+    REFERENCES petshop(id);
+
+ALTER TABLE petshop_exame ADD CONSTRAINT fk_id_exame_petshop_exame
+    FOREIGN KEY (id_exame)
+    REFERENCES exame(id);
+
+ALTER TABLE animal_cliente ADD CONSTRAINT fk_id_animal_animal_cliente
+    FOREIGN KEY (id_animal)
+    REFERENCES animal(id);
+
+ALTER TABLE animal_cliente ADD CONSTRAINT fk_id_cliente_animal_cliente
+    FOREIGN KEY (id_cliente)
+    REFERENCES cliente(id);
